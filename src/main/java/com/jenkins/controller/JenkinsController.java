c7e6255a8cb6c074e2a69f98951184b4e20d7b93
sanket khose <sanket.khose@bajajfinserv.in>
2024-06-21 13:02:40 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -0,0 +1,18 @@
+package com.jenkins.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+	
+	@GetMapping("/v1/jenkins-data")
+	public ResponseEntity<String> getName() {
+
+		return new ResponseEntity<>("Jenkins is Configure and its working", HttpStatus.OK);
+	}
+
+}
